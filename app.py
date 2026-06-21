400d371c25484fa3deda321decb50bf673f1ce55
jasundtv-maker <jasundtv@gmail.com>
2026-06-21 23:32:16 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="JASUND.NET BILLING",
+    page_icon="📡",
+    layout="wide"
+)
+
+st.title("📡 JASUND.NET BILLING")
+
+st.success("Aplikasi Billing dan Invoice Otomatis JASUND.NET berhasil berjalan.")
